d4fca46033b60189ed1273d6d3352d646b74caa1
bzaf-o <balqiszafirah.o@gmail.com>
2024-11-20 20:50:08 +0800

    Update kmk2553_w7.py

--- kmk2553_w7.py
+++ kmk2553_w7.py
@@ -1,7 +1,7 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 
 # Title
 st.title("🌟 Interactive Mental Health Modelling Tool")
